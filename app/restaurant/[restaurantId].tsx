1c170f82928e5cc9c08103c2809aebf0599774e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-15 15:48:59 +0300

    Added some stack screen options to the Hotel Details page

--- app/restaurant/[restaurantId].tsx
+++ app/restaurant/[restaurantId].tsx
@@ -1,11 +1,20 @@
 import RestaurantDetails from '@/components/restaurant-details'
-import { useLocalSearchParams } from 'expo-router'
+import { Stack, useLocalSearchParams } from 'expo-router'
 import React from 'react'
 
 const RestaurantDetailPage = () => {
 	const { restaurantId } = useLocalSearchParams()
 
-	return <RestaurantDetails restaurantId={restaurantId as string} />
+	return (
+		<>
+			<Stack.Screen
+				options={{
+					headerTitle: 'Hotel Details'
+				}}
+			/>
+			<RestaurantDetails restaurantId={restaurantId as string} />
+		</>
+	)
 }
 
 export default RestaurantDetailPage
