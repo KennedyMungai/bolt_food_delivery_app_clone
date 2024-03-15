26461b69fa7385656cb4a599e80758cbc1d3f32b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-15 15:56:29 +0300

    Made some changes

--- app/restaurant/[restaurantId].tsx
+++ app/restaurant/[restaurantId].tsx
@@ -1,9 +1,23 @@
+import { dummyRestaurantsData } from '@/assets/data/restaurantsData'
 import RestaurantDetails from '@/components/restaurant-details'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 const RestaurantDetailPage = () => {
 	const { restaurantId } = useLocalSearchParams()
+
+	const details = dummyRestaurantsData.find(
+		(item) => item.id === restaurantId
+	)
+
+	if (!details) {
+		return (
+			<View style={styles.notFoundContainer}>
+				<Text style={styles.notFoundText}>Hotel Details not found</Text>
+			</View>
+		)
+	}
 
 	return (
 		<>
@@ -12,9 +26,23 @@ const RestaurantDetailPage = () => {
 					headerTitle: 'Hotel Details'
 				}}
 			/>
-			<RestaurantDetails restaurantId={restaurantId as string} />
+			<RestaurantDetails details={details} />
 		</>
 	)
 }
 
 export default RestaurantDetailPage
+
+const styles = StyleSheet.create({
+	notFoundContainer: {
+		flex: 1,
+		backgroundColor: '#F85',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	notFoundText: {
+		fontSize: 24,
+		fontWeight: 'bold'
+	}
+})
