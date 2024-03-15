fa8cb12ef36124c18ba6a6e0075a568c8e5b6a05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-15 16:27:52 +0300

    Gave the image a resize mode of cover

--- components/restaurant-details.tsx
+++ components/restaurant-details.tsx
@@ -15,6 +15,7 @@ const RestaurantDetails = ({ details }: Props) => {
 				<Image
 					source={{ uri: details.profileImage }}
 					style={{ width: '100%', height: '100%' }}
+					resizeMode='cover'
 				/>
 			)}
 		></ParallaxScrollView>
