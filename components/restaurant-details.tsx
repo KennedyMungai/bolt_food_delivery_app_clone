49faa7b1092dfa6575ca078ae8619425d37fe32b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-15 16:00:06 +0300

    Added a simple orange tint to the hotels details page

--- components/restaurant-details.tsx
+++ components/restaurant-details.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 type Props = {
@@ -7,10 +7,17 @@ type Props = {
 
 const RestaurantDetails = ({ details }: Props) => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>{details.id}</Text>
 		</View>
 	)
 }
 
 export default RestaurantDetails
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#FF885525'
+	}
+})
