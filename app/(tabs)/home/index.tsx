9ff8837f352d8e98e9a8003b16356a153a2e9f51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-15 14:32:11 +0300

    Added some markup and styling to the Home Page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
 import React from 'react'
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons'
 
 const TabsHomePage = () => {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text>TabsHomePage</Text>
+			<View style={styles.header}>
+				<View style={styles.addressContainer}>
+					<MaterialCommunityIcons name='map-marker' size={28} />
+					<Text style={styles.addressText}>Your Address Here</Text>
+				</View>
+			</View>
 		</SafeAreaView>
 	)
 }
@@ -15,8 +21,21 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
+		padding: 20
+	},
+	header: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 20
+	},
+	addressContainer: {
+		display: 'flex',
+		flexDirection: 'row',
 		justifyContent: 'center',
-		paddingVertical: 20
+		alignItems: 'center'
+	},
+	addressText: {
+		marginLeft: 10
 	}
 })
