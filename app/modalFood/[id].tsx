cb501d95f85bab528d62f0e73910ec6580bd42bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 11:23:45 +0300

    Created the route for the food details

---
+++ app/modalFood/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FoodDetailsModal = () => {
+	return (
+		<View>
+			<Text>FoodDetailsModal</Text>
+		</View>
+	)
+}
+
+export default FoodDetailsModal
