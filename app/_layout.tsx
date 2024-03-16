99dbcb882bed9bb6dceb6bc59fd0d4395d5943cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 11:23:23 +0300

    Added the route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,10 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome'
-import { useFonts } from 'expo-font'
-import { Stack } from 'expo-router'
-import { useEffect } from 'react'
-
-export {
-	// Catch any errors thrown by the Layout component.
-	ErrorBoundary
-} from 'expo-router'
-
-export const unstable_settings = {
-	// Ensure that reloading on `/modal` keeps a back button present.
-	initialRouteName: '(tabs)'
-}
-
-export default function RootLayout() {
-	const [loaded, error] = useFonts({
-		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-		...FontAwesome.font
-	})
-
-	// Expo Router uses Error Boundaries to catch errors in the navigation tree.
-	useEffect(() => {
-		if (error) throw error
-	}, [error])
-
-	if (!loaded) {
-		return null
-	}
-
-	return <RootLayoutNav />
-}
+import { Ionicons } from '@expo/vector-icons'
+import { Stack, useNavigation } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 
 function RootLayoutNav() {
+	const navigation = useNavigation()
+
 	return (
 		<Stack>
 			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
@@ -40,6 +13,30 @@ function RootLayoutNav() {
 				options={{
 					animation: 'slide_from_right',
 					animationDuration: 300
+				}}
+			/>
+			<Stack.Screen
+				name='modalFood/[id]'
+				options={{
+					presentation: 'modal',
+					headerTitle: '',
+					headerTransparent: true,
+					headerRight: () => (
+						<TouchableOpacity
+							style={{
+								backgroundColor: 'white',
+								borderRadius: 20,
+								padding: 2
+							}}
+							onPress={() => navigation.goBack()}
+						>
+							<Ionicons
+								name='close-outline'
+								size={30}
+								color={'#5A5A5A'}
+							/>
+						</TouchableOpacity>
+					)
 				}}
 			/>
 		</Stack>
