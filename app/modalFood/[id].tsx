8bbb8de3b8845dddeaba0bbf336cb0f598d01a2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 11:28:16 +0300

    Styled the modal component page

--- app/modalFood/[id].tsx
+++ app/modalFood/[id].tsx
@@ -1,12 +1,20 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
 
 const FoodDetailsModal = () => {
 	return (
-		<SafeAreaView>
+		<SafeAreaView style={styles.container}>
 			<Text>FoodDetailsModal</Text>
 		</SafeAreaView>
 	)
 }
 
 export default FoodDetailsModal
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#F8F8F850',
+		flex: 1,
+		padding: 10
+	}
+})
