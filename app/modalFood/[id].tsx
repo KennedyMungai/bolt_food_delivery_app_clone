4a8bbdcf2b8e4ca28ff23d856cb7ebb68d516edb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-16 11:37:07 +0300

    Console logged out the foundMeall items

--- app/modalFood/[id].tsx
+++ app/modalFood/[id].tsx
@@ -1,10 +1,32 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { dummyRestaurantsData } from '@/assets/data/restaurantsData'
+import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
+import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
 
 const FoodDetailsModal = () => {
+	const { id, restaurantId } = useLocalSearchParams()
+
+	const restaurantById = dummyRestaurantsData.find(
+		(r) => r.id === restaurantId
+	)
+
+	const meals: Meal[] | undefined = restaurantById?.food.flatMap(
+		(c) => c.meals
+	)
+
+	const foundMeals: Meal | undefined = meals?.find((m) => m.id === +id)
+
+	console.log(foundMeals)
+
 	return (
 		<SafeAreaView style={styles.container}>
-			<Text>FoodDetailsModal</Text>
+			<View style={styles.someContainer}>
+				<Image
+					source={{ uri: '' }}
+					resizeMode='contain'
+					style={styles.imageContainer}
+				/>
+			</View>
 		</SafeAreaView>
 	)
 }
@@ -16,5 +38,13 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F8F8F850',
 		flex: 1,
 		padding: 10
+	},
+	someContainer: {
+		backgroundColor: 'white',
+		borderRadius: 20
+	},
+	imageContainer: {
+		width: '100%',
+		height: 400
 	}
 })
